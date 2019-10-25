3ab5c1819b4b44805bc9148da41a15888196372f
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-25 14:11:19 +0200

    Opdracht 1 hello world applicatie package hernoemd, opdracht 2 sum klaar, opdracht 3 multiplicatie klaar

---
+++ opdrachten/src/Opdracht3.java
@@ -0,0 +1,10 @@
+public class Opdracht3 {
+    public static void main(String[] args){
+        int sum = 1;
+
+        for (int a = 2; a <=10; a++){
+            sum *= a;
+        }
+        System.out.println(sum);
+    }
+}
