c1f31d09f5a2f980a4dfdbcddcd3618e01dfb076
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-25 15:54:07 +0200

    opdracht 11, 3 favoriete dingen geprint naar console

---
+++ opdrachten hoofdstuk 1/src/Opdracht11.java
@@ -0,0 +1,9 @@
+public class Opdracht11 {
+    public static void main(String[] args){
+        System.out.println("Chair");
+        System.out.println("house");
+        System.out.println("table");
+
+
+    }
+}
