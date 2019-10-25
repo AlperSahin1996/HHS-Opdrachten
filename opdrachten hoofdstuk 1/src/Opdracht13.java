fd1530f0f433bbb1c25a5bf3fdbc6879c15e5544
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-25 16:01:59 +0200

    opdracht 13, usa flag naar console

---
+++ opdrachten hoofdstuk 1/src/Opdracht13.java
@@ -0,0 +1,10 @@
+public class Opdracht13 {
+    public static void main(String[] args){
+        System.out.println("*****==============");
+        System.out.println("*****==============");
+        System.out.println("*****==============");
+        System.out.println("===================");
+        System.out.println("===================");
+
+    }
+}
