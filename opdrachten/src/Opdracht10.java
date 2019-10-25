4110deb7cf088080f42b3bb7a940d9d3214ed1f7
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-25 15:39:43 +0200

    opdracht 10 kat gemaakt naar console

---
+++ opdrachten/src/Opdracht10.java
@@ -0,0 +1,10 @@
+public class Opdracht10 {
+    public static void main(String[] args){
+        System.out.println("          _,\'|             _.-''``-...___..--';)");
+        System.out.println("         /_ \\'.      __..-' ,      ,--...--\'\'\'");
+        System.out.println("Meoow!  <\\    .`--'''       `     /\'");
+        System.out.println("         `-';'               ;   ; ;");
+        System.out.println("   __...--''     ___...--_..'  .;.'");
+        System.out.println("  (,__....----\'\'\'       (,..--\'\' ");
+    }
+}
