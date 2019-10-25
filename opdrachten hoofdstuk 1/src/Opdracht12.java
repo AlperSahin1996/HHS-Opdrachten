0e170ee1ae7d6cfb3596e7e8d38832b5562426bc
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-25 15:59:39 +0200

    opdracht 12, 3 favoriete poem naar console

---
+++ opdrachten hoofdstuk 1/src/Opdracht12.java
@@ -0,0 +1,8 @@
+public class Opdracht12 {
+    public static void main(String[] args){
+        System.out.println("I thought I had forgotten,");
+        System.out.println("But it all came back again");
+        System.out.println("To-night with the first spring thunder");
+        System.out.println("In a rush of rain.");
+    }
+}
