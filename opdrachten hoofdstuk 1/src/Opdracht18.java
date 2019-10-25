448055765f946a071babac39319aaeb3678973f4
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-25 16:46:49 +0200

    opdracht 18, business 2 colums met 3 rijen met naam en datum

---
+++ opdrachten hoofdstuk 1/src/Opdracht18.java
@@ -0,0 +1,8 @@
+public class Opdracht18 {
+    public static void main(String[] args){
+        System.out.println("| Alper | 01.01.2000 |");
+        System.out.println("| Alper | 01.01.2000 |");
+        System.out.println("| Alper | 01.01.2000 |");
+
+    }
+}
