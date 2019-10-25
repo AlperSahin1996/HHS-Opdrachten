0b2363a6801ace9622cde3c515a305ae5e11517a
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-25 16:16:16 +0200

    opdracht 15, swing message (popup button met string naam) met invultab

---
+++ opdrachten hoofdstuk 1/src/Opdracht15.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Opdracht15 {
+    public static void main(String[] args){
+        String name = JOptionPane.showInputDialog("What is your name?");
+        System.out.println(name);
+    }
+}
