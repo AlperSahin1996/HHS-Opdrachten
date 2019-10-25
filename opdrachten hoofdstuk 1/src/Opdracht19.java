77c0c2b706bec32ee86c748b64fc4edff2cd627d
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-25 16:52:38 +0200

    opdracht 19, business sales tax rates met rijen en columns van  5 staten

---
+++ opdrachten hoofdstuk 1/src/Opdracht19.java
@@ -0,0 +1,13 @@
+public class Opdracht19 {
+    public static void main(String[] args){
+        System.out.println("Sales Tax Rates");
+        System.out.println("---------------");
+        System.out.println("Alaska:      0%");
+        System.out.println("Hawaii:      2%");
+        System.out.println("New York:   19%");
+        System.out.println("California:  4%");
+        System.out.println("Nevada:      7%");
+
+
+    }
+}
