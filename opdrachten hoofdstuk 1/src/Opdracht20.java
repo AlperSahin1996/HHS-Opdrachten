10344e8b1c5b1a1b8947ceca906f73bb3af35eda
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-25 16:55:33 +0200

    opdracht 20, business groeten van engels naar nl columns

---
+++ opdrachten hoofdstuk 1/src/Opdracht20.java
@@ -0,0 +1,9 @@
+public class Opdracht20 {
+    public static void main(String[] args){
+        System.out.println("         List of Phrases to Translate");
+        System.out.println("Good Morning.               || Goeiemorgen.");
+        System.out.println("It is pleasure to meet you. || Fijn om kennis met je te maken.");
+        System.out.println("Please call me tommorow.    || Bel me morgen alsjeblieft.");
+        System.out.println("Have a nice day.            || Nog een fijne dag.");
+    }
+}
