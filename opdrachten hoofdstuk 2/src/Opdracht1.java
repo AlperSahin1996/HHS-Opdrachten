1d271d33f5a6d7cde8e906e6d72a8f797d857ffa
AlperSahin1996 <alpersahin1996@hotmail.com>
2019-10-26 15:24:42 +0200

    opdracht 1 hoofdstuk 2 klaar. inches naar millimeters

--- opdrachten hoofdstuk 2/src/Opdracht1.java
+++ opdrachten hoofdstuk 2/src/Opdracht1.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Opdracht1 {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        final double MILLIMETERS_PER_INCH  = 25.4;
+
+        System.out.println("Enter height: ");
+        double height = scanner.nextDouble();
+        double height2 = MILLIMETERS_PER_INCH * height;
+
+        System.out.println("Enter width: ");
+        double width = scanner.nextDouble();
+        double width2 = MILLIMETERS_PER_INCH * width;
+
+        System.out.println("Dimension of the letter is: ");
+        System.out.printf("%.2f - height \n", height2);
+        System.out.printf("%.2f - width \n", width2);
+
+
+
 
     }
 }
